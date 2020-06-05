ddeae7cc1e871d56d4a2e591e42e31f52f16f5c4
Andy Yee <andyych88@gmail.com>
2020-06-05 02:35:25 -0400

    add web entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Running openmid web docker entrypoint script"
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
